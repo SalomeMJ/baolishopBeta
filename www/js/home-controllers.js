6e3c7fc8dd429a7a2fb1de46aa78be3db74a4c7e
SalomeMJ <2825027346@qq.com>
2017-12-08 16:49:16 +0800

    wancheng

--- www/js/home-controllers.js
+++ www/js/home-controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers.home', [])
 
-  .controller('homeCtrl', function($scope,$stateParams,$location,Goods,$state,$rootScope) {
+  .controller('homeCtrl', function($scope,$stateParams,$location,Goods,Says,$state,$rootScope) {
     $scope.title = '家居';
     //头部按钮
     $('.btns').each(function(i){
@@ -22,20 +22,35 @@ angular.module('starter.controllers.home', [])
 
     $scope.add=function(item){
       item.count++;
-      // console.log(item)
-      // console.log(item.id);
+      console.log(item.count);
       $('.decrease')[item.id-1].style.display='block';
       $('.number')[item.id-1].style.display='block';
       $('.total').css('display','block');
       $('.well').css('display','block');
       $('.please').css('display','none');
-      // $('.num')[0].innerHTML=parseInt($('.number')[item.id-1].innerHTML)+1;
       $('.num')[0].innerHTML=parseInt($('.number')[0].innerHTML)+1+parseInt($('.number')[1].innerHTML)
         +parseInt($('.number')[2].innerHTML)+parseInt($('.number')[3].innerHTML)
         +parseInt($('.number')[4].innerHTML)+parseInt($('.number')[5].innerHTML)
         +parseInt($('.number')[6].innerHTML);
       $('.mon')[0].innerHTML='￥'+$('.num')[0].innerHTML*1988;
-    };
+
+      //  将用户购买的东西通过$routeScope传给确认订单的送水服务
+      var arr=[];
+      for(var index=0;index<$scope.goods.length;index++){
+        if($scope.goods[index].count>0){
+          arr.push($scope.goods[index]);
+        }
+      }
+      $rootScope.codes=arr;
+      $rootScope.total=$('.mon')[0].innerHTML;
+
+
+      // console.log(item.id);
+      // $rootScope.services=item.title;
+      // $rootScope.servicesPrice=item.price;
+      // $rootScope.numb=item.count;
+      // console.log($rootScope.servicesPrice+'*'+$rootScope.numb)
+    }
     $scope.desc=function(item){
       item.count--;
       // console.log(item)
@@ -57,9 +72,10 @@ angular.module('starter.controllers.home', [])
     }
 
     $scope.goods = Goods.all();
-    $scope.remove = function(good) {
-      Goods.remove(good);
-    };
+    console.log(Goods.all())
+
+
+
     //  选项卡
     $('.btns').each(function(i){
       $('.btns').eq(i).click(function(){
@@ -81,11 +97,18 @@ angular.module('starter.controllers.home', [])
     $scope.decide=function(){
       $state.go('tab.decideCode');
     }
+  //  评价部分
+    $scope.says=Says.all();
+
+
 
   })
   //订单
-  .controller('decideCtrl',function($scope,$state){
-
+  .controller('decideCtrl',function($scope,$state,$rootScope){
+    $scope.codes=$rootScope.codes;
+    $scope.totalMon=$rootScope.total;
+    // console.log($scope.codes);
+    // console.log($scope.codes[0].count)
     $scope.goWay=function(){
       $state.go('tab.paywayCode');
     }
